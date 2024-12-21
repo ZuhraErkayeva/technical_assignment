b389aa62b9d5fdc82269bbfef4d9177b8c403596
Zuhra <erkayevazuhra6@gmail.com>
2024-12-21 23:34:27 +0300

    i'm created urls

---
+++ posts/urls.py
@@ -0,0 +1,20 @@
+from .views import UserViewSet,BlogPostViewSet,CommentViewSet
+from rest_framework.routers import DefaultRouter
+from django.urls import path
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
+router = DefaultRouter
+router.register('user', UserViewSet),
+router.register('posts', BlogPostViewSet),
+router.register('comments', CommentViewSet),
+
+urlpatterns += router.urls
+
+
