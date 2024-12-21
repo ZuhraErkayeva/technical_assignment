5249f9b837d5e0ef9a2765e6e9322454e147ef29
Zuhra <erkayevazuhra6@gmail.com>
2024-12-21 22:19:02 +0300

    i'm created model for Posts

--- posts/models.py
+++ posts/models.py
@@ -7,3 +7,10 @@ class User(AbstractUser):
 
 
 
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
