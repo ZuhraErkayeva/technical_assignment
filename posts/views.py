557e273b836542e7636050c73c5d6c9e0cb31d70
Zuhra <erkayevazuhra6@gmail.com>
2024-12-21 23:26:15 +0300

    i'm created view for user, blogpost and comments

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import User,BlogPost,Comment
+from .serializers import UserSerializers,BlogPostSerializers,CommentSerializer
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializers
+
+class BlogPostViewSet(ModelViewSet):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializers
+
+class CommentViewSet(ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
