2b173597878bc3fc2b7faea607911792c7b78a12
Zuhra <erkayevazuhra6@gmail.com>
2024-12-21 22:18:44 +0300

    i'm created model for User

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=20, choices=[('user', 'User'), ('admin', 'Admin')], default='user')
+
+
+
