59a6c66a905d7adace1585a1e25b3fa79275ed54
Zuhra <erkayevazuhra6@gmail.com>
2024-12-21 23:23:56 +0300

    i'm created serializer for blogpost and i added count function

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import User, BlogPost
+from .models import User, BlogPost, Comment
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 
@@ -10,7 +10,6 @@ class UserSerializers(serializers.ModelSerializer):
 
 
 
-
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
@@ -18,3 +17,27 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         token['id'] = user.id
         token['role'] = user.role
         return token
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+
+    class Meta:
+        model = Comment
+        fields = ['post','author','content','created_at']
+
+    def create(self, validated_data):
+        validated_data['author'] = self.context['request'].user
+        return super().create(validated_data)
+
+
+class BlogPostSerializers(serializers.ModelSerializer):
+    post_count = serializers.SerializerMethodField
+    comments = CommentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = BlogPost
+        fields = ('title','content','is_published','post_count')
+
+    def get_post_count(self, obj):
+        return obj.comments.count()
